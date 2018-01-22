a2c9d7a2a4959730485b67a8c9853b0297d1b7e5
wangjianfeng <805455985@qq.com>
2018-01-22 16:07:50 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by wangjianfeng on 2017/12/26.
+ *
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world";
+    }
+
+}
